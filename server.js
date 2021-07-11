9fcf99f45f121d2ad80286ee0c96356369e1621b
moiz khan <moiz.khan@applligent.com>
2021-07-11 13:41:26 +0530

    video streaming app

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+
+    console.log(roomId, userId, userName)
+    socket.join(roomId);
+
+    socket
+    .broadcast
+    .to(roomId)
+    .emit("user-connected", userId);
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+      console.log(message)
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
